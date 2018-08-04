6ad20024338aa26ed374fba063fcd60dbd2e90d9
Vadim Fedorov <bhoodream@yandex.ru>
2018-08-04 14:45:24 +0700

    window.jQuery = $;

--- addChameleonJsToJquery.js
+++ addChameleonJsToJquery.js
@@ -1,9 +1,9 @@
 module.exports = $ => {
-    const jQuery = $;
-
     if (typeof window === 'undefined') {
         const window = typeof global !== 'undefined' ? global : {};
     }
+
+    window.jQuery = $;
 
     require('./jquery.chameleon');
 };
